7d9395b146e2ac50dd38cd69204a6f60347805ba
Fajar Siagian <fajar7xx@gmail.com>
2025-11-09 23:33:02 +0700

    Add blog functionality with content configuration and initial posts

---
+++ src/content.config.ts
@@ -0,0 +1,24 @@
+// import glob loader
+import { glob } from 'astro/loaders';
+
+// import utilities from astro:content
+import { z, defineCollection } from 'astro:content';
+
+// define a loader and schema for each collection
+const blog = defineCollection({
+    loader: glob({ pattern: '**/[^_]*.md', base: "./src/blog"}),
+    schema: z.object({
+        title: z.string(),
+        pubDate: z.date(),
+        description: z.string(),
+        author: z.string(),
+        image: z.object({
+            url: z.string(),
+            alt: z.string(),
+        }),
+        tags: z.array(z.string()),
+    }),
+});
+
+// export a single collection object to register your collection(s)
+export const collections = { blog };
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,20 @@
 import rss, {pagesGlobToRssItems} from "@astrojs/rss";
+import { getCollection } from "astro:content";
 
 export async function GET(context){
+    const posts = await getCollection("blog");
     return rss({
         title: "My Astro Blog",
         description: "My Astro Blog",
         site: context.site,
-        items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
-        customData: "<language>en-us</language>",
-    })
+        items: posts.map((post) => ({
+            title: post.data.title,
+            link: `${context.site.url}/posts/${post.id}`,
+            pubDate: post.data.pubDate,
+            description: post.data.description,
+            author: post.data.author,
+            categories: post.data.tags,
+        })),
+    customData: "<language>en-us</language>",
+  });
 }
